9353c46e669fdbd5a37eb2aa3ce96987b4323fde
Elson <elsouzalopes@gmail.com>
2025-11-03 16:35:22 -0300

    Adicionar arquivos de projeto.

---
+++ CryptographySysMiddle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LayoutParserLib
+{
+    public class CryptographySysMiddle
+    {
+        private static readonly SymmetricAlgorithm SymmetricAlgorithm = new RijndaelManaged();
+
+        public static string Decrypt(string messageToDecrypt)
+        {
+            ICryptoTransform cryptoTransform = SymmetricAlgorithm.CreateDecryptor(Encoding.UTF8.GetBytes("dbc%$#h92785"), Encoding.UTF8.GetBytes("Ca#&UjO){Qwz*@FcsPs"));
+            byte[] inputBytes = Convert.FromBase64String(messageToDecrypt);
+            byte[] bytes = Operation(cryptoTransform, inputBytes);
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        private static byte[] Operation(ICryptoTransform cryptoTransform, byte[] inputBytes)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write);
+            cryptoStream.Write(inputBytes, 0, inputBytes.Length);
+            cryptoStream.Close();
+            return memoryStream.ToArray();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("LayoutParserLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NDD")]
+[assembly: AssemblyProduct("LayoutParserLib")]
+[assembly: AssemblyCopyright("Copyright © NDD 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("321e1346-e8cb-46f8-a14e-f200212a7630")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
